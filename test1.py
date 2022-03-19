d7c779bf9331b335d130e123cf51cf5cf646bb64
indraone01 <indraone01@gmail.com>
2022-03-19 21:33:15 +0700

    takeout unnecessary file

--- test1.py
+++
@@ -1,7 +0,0 @@
-from qiskit import IBMQ
-
-#IBMQ.save_account('5d03c00b85463406be8cb6780d04819999d213516e5ae62228632ea7b9a5f2892df800442bde9ebac610dfd8dbf76050aa9fb269c42723be90e72193e363e0de')
-#print(IBMQ.stored_accounts())
-#print(IBMQ.active_accounts())
-provider = IBMQ.load_account()
-backend = provider.get_backend('ibmq_qasm_simulator')
